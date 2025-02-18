4cda89af37038c01c44317b1c4a68f1b658af677
MrShockWAVEE <jadly129131@gmail.com>
2025-02-18 19:23:58 +0600

    Update cmdline.c

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -11,13 +11,13 @@ extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 	#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-		if (!susfs_spoof_cmdline_or_bootconfig(m)) {
-			seq_putc(m, '\n');
-			return 0;
-		}
-	#endif
-		 seq_printf(m, "%s\n", saved_command_line);
-		 return 0;
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
+ 	seq_printf(m, "%s\n", saved_command_line);
+ 	return 0;
 }
 
 static int cmdline_proc_open(struct inode *inode, struct file *file)
